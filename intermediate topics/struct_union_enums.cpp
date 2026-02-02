320f91518c454a360bec4103136b20c77ffc15c0
Aryan Gupta <guptaaryan4501@gmail.com>
2026-02-02 21:37:35 +0530

    add: structs, unions and enums
    Just completed a tutorial on how to use structs, unions and enums.

--- intermediate topics/struct_union_enums.cpp
+++ intermediate topics/struct_union_enums.cpp
@@ -2,15 +2,34 @@
 
 using namespace std;
 
-struct employee
+typedef struct employee
 {
     int id;
     char favchar;
     float salary;
+} ep;
+
+union money
+{
+    int mangoes;
+    float rupees;
+    char car;
 };
 
 int main(){
+    enum meal{breakfast, lunch, dinner};
+    meal m = lunch;
+    cout<<m<<endl;
+    cout<<breakfast<<endl;
+    cout<<lunch<<endl;
+    cout<<dinner<<endl;
     struct employee aryan;
+    union money m1;
+    m1.mangoes = 6346;
+    m1.car = 'v';
+    cout<<"The value of mangoes is "<<m1.mangoes<<endl;
+    cout<<"The value of car is "<<m1.car<<endl;
+    ep lakshya;
     aryan.id = 9;
     aryan.favchar = 'A';
     aryan.salary = 4000000000;
