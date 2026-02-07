8bafde6c7907d6adbc9271aa60d3f6500910c0fe
Aryan Gupta <guptaaryan4501@gmail.com>
2026-02-07 20:09:19 +0530

    add: recursions
    learned how recursions work and how to use them.

--- intermediate topics/recursion.cpp
+++ intermediate topics/recursion.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n){
+    if(n<2){
+        return 1;
+    }
+    return fib(n-2) + fib(n-1);
+}
+int factorial(int n){
+    if(n<=1){
+        return 1;
+    }
+    return n* factorial(n-1);
+}
 int main(){
-    
+    // Factorial number
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"The factorial of "<<a<<" is: "<<factorial(a)<<endl;
+    cout<<"The Fibonacci of "<<a<<" is: "<<fib(a)<<endl;
     return 0;
 }
